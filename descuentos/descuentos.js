ab248a1d09290c9ac747f093d0d6ea107da712fb
rodrigo203203 <rodrigo-morales2011@hotmail.es>
2022-05-18 14:49:00 -0400

    funcion con cupones

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,3 +1,4 @@
+const coupons = ["MICODIGO23","DESCUENTO2021","MAMA2022"];
 function calcularPrecioConDescuento (precio,descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
@@ -6,8 +7,20 @@ function calcularPrecioConDescuento (precio,descuento){
 function respuestaConDescuento(){
     const inputPrecio = document.getElementById("InputPrice");
     const precio = inputPrecio.value;
-    const inputDescuento = document.getElementById("InputDescuento");
-    const descuento = inputDescuento.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    let descuento;
+    switch (couponValue) {
+        case coupons[0]:
+            descuento = 15;
+        break;
+        case coupons[1]:
+            descuento = 30;
+        break;
+        case coupons[2]:
+            descuento = 25;
+        break;
+    }
     const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
     const resultP = document.getElementById("ResultP"); 
     resultP.innerText = "El precio con descuento es de $" + precioConDescuento;
